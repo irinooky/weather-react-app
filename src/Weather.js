d75f38f205d305215922768004d9b62fc5a23b4d
irinooky <bernaz.irina@gmail.com>
2022-12-13 14:37:07 +0100

    changed color of the animation

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       height="80"
       width="80"
       radius="9"
-      color="yellow"
+      color="white"
       ariaLabel="loading"
       wrapperStyle
       wrapperClass
