09b1ff63400fdee494ae705b979cf96888da6787
irinooky <bernaz.irina@gmail.com>
2022-12-15 12:34:58 +0100

    added index.js and weather.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import Weather from "./Weather";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="New York" />
-      </header>
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{""}
+        <a href="instagram.com/irinooky/" target="_blank">
+          {" "}
+          Irina Bernaz
+        </a>{" "}
+        and is
+        <a href="https://github.com/irinooky/weather-react-app" target="_blank">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
