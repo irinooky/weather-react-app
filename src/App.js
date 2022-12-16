36ec1827c86a69f6275ff05c8670f15aab69f00f
irinooky <bernaz.irina@gmail.com>
2022-12-16 12:37:13 +0100

    added bootstrap and improved html

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{""}
-        <a href="instagram.com/irinooky/" target="_blank">
-          {" "}
-          Irina Bernaz
-        </a>{" "}
-        and is
-        <a href="https://github.com/irinooky/weather-react-app" target="_blank">
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project is created by{""}
+          <a href="instagram.com/irinooky/" target="_blank">
+            {" "}
+            Irina Bernaz
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/irinooky/weather-react-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
