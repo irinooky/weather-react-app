fdaaf942d0f0fd0d55d171b1130e3ed3bea09a63
irinooky <bernaz.irina@gmail.com>
2022-12-21 13:23:18 +0100

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -11,9 +11,18 @@ export default function WeatherForecastDay(props) {
     return `${temperature}°`;
   }
 
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
-      <div className="WeatherForecast-day">{props.data.dt}"</div>
+      <div className="WeatherForecast-day">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={60} />
 
       <div className="WeatherForecast-temperature">
