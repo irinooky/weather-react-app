66f7ad22b5732620cea5d5733f35f1cccaff6454
irinooky <bernaz.irina@gmail.com>
2022-12-21 12:10:41 +0100

    added forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,7 +15,7 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row">
         <div className="col-6 d-flex">
-          <WeatherIcon code={props.data.icon} />
+          <WeatherIcon code={props.data.icon} size={props.size} />
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
