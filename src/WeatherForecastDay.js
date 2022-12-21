65cded4a2655b128198e3fbf83420ba583a32c57
irinooky <bernaz.irina@gmail.com>
2022-12-21 13:17:32 +0100

    added forecast max and min temp

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,29 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  return (
+    <div>
+      <div className="WeatherForecast-day">{props.data.dt}"</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={60} />
+
+      <div className="WeatherForecast-temperature">
+        <span className="WeatherForecast-temperature-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperature-min">
+          {minTemperature()}
+        </span>
+      </div>
+    </div>
+  );
+}
